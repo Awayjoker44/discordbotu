9e93114a9188c8a83d16ce2923cf0edddfb64cfe
Awayjoker44 <awayjoker44@gmail.com>
2025-02-10 00:47:17 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 set -e
 
-# Railway tarafından atanmış PORT'u kullan, yoksa varsayılan 4444
+# Railway ortamında PORT ortam değişkeni varsa onu kullan, yoksa varsayılan 4444
 PORT_NUM=${PORT:-4444}
 
 echo "Selenium sunucusunu $PORT_NUM portunda başlatıyoruz..."
+# Selenium sunucusunu arka planda başlatıyoruz
 java -jar /opt/selenium-server.jar standalone --port=$PORT_NUM &
 
 echo "Selenium sunucusunun hazır olmasını bekliyoruz..."
